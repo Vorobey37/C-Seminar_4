3ca5943f93707e36ccfcb4f16a6db0551ed81e59
Abvorobey <abvorobey@gmail.com>
2023-02-19 23:46:53 +0300

    Сумма чисел методом

---
+++ Sum_Metod/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите прошрамму, которая на вход принимает число А,
+а на выходе выдает сумму чисел от 1 до А.
+*/
+Console.Clear();
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine());
+int Sum(int A)
+{
+    int sum = 0;
+    for (int i=1; i<=A; i++)
+    {
+        sum += i;
+    }
+return sum;
+}
+Console.WriteLine (Sum (A));
