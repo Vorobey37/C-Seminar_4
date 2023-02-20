cf83c4c3b8f84f98a0703bd31058fd04310d5257
Abvorobey <abvorobey@gmail.com>
2023-02-20 23:34:01 +0300

    Количество цифр в числе

---
+++ How_Many_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает число, 
+а на выходе показывает сколько цифр у данного числа.
+*/
+Console.Clear();
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine());
+int Number(int A)
+{
+    int count = 1;
+    if (A<0)
+    {
+        A*=-1;
+    }
+    while (A>9)
+    {
+        A/=10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(Number(A));
