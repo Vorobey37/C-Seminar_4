b615a8a2fe8416bf2b1addaa37804c6a743d2a74
Abvorobey <abvorobey@gmail.com>
2023-02-21 22:57:15 +0300

    Метод создания массива

---
+++ Mass_Random_0_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов,
+заполненный 0 и 1 в случайном порядке.
+*/
+Console.Clear();
+int N = 8;
+int [] MassRandom(int N)
+{
+    int [] mass = new int [N];
+    Console.Write("[");
+    for (int i=0; i<(N-1); i++)
+    {
+        mass[i] = new Random().Next(0,2);
+        Console.Write($"{mass[i]},");
+    }
+    Console.Write($"{mass[(N-1)]}]");
+    return mass;
+}
+int [] b = MassRandom (N);
